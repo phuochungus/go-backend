00301bed49aa2da41036a9a613af5b14cb20fa8c
HungNguyen <phuochungus@gmail.com>
2026-08-31 16:06:33 +0700

    feat: add local docker-compose for mysql and redis and comment out user routes

--- internal/routers/manage/user.router.go
+++ internal/routers/manage/user.router.go
@@ -5,9 +5,9 @@ import "github.com/gin-gonic/gin"
 type UserRouter struct{}
 
 func (ur *UserRouter) InitUserRouter(router *gin.RouterGroup) {
-	userPrivateRouter := router.Group("/admin/user")
-	{
-		userPrivateRouter.POST("/active_user")
-	}
+	// userPrivateRouter := router.Group("/admin/user")
+	// {
+	// 	userPrivateRouter.POST("/active_user")
+	// }
 
 }
